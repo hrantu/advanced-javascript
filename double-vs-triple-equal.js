db71d8aefd6ad4c8c51ce1ca8bb6ebde257eeb81
hrantu <md.hazzaz.rahman.antu@g.bracu.ac.bd>
2020-12-20 03:18:16 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,15 @@
+const first = 2;
+const second = "2";
+if(first == second){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+const third = 2;
+const forth = "2";
+if(third === forth){
+    console.log("2.condition is true");
+}else{
+    console.log("2.condition is false");
+}
